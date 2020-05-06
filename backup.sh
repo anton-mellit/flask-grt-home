9706aa29be0d97c79589e969d1cc31cf0125aecc
anton-mellit <mellit@gmail.com>
2020-05-06 13:28:51 +0200

    scripts

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 NOW=$(date +"%Y-%m-%d")
 FILE="backup/backup-$NOW.tar.gz"
+LOGFILE="logs/log-backup-$NOW.log"
 
-tar -cvzf $FILE data
-
+tar -cvzf $FILE data > $LOGFILE
--- send_digest.sh
+++ send_digest.sh
@@ -1,5 +1,4 @@
 NOW=$(date +"%Y-%m-%d")
 LOGFILE="logs/log-digest-$NOW.log"
 
-python3.8.2 send_digest.py | tee $LOGFILE
-
+python3.8.2 send_digest.py > $LOGFILE
