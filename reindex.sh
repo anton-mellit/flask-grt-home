3e12540ccd41e80444f31fbfcc017a3fe9020701
anton-mellit <mellit@gmail.com>
2020-05-10 01:00:57 +0200

    Redirect stderr too

--- reindex.sh
+++ reindex.sh
@@ -1,4 +1,4 @@
 NOW=$(date +"%Y-%m-%d-%H")
 LOGFILE="logs/www/log-reindex-$NOW.log"
 
-python3.8.2 search.py > $LOGFILE
+python3.8.2 search.py > $LOGFILE 2>&1
--- send_digest.sh
+++ send_digest.sh
@@ -1,4 +1,4 @@
 NOW=$(date +"%Y-%m-%d")
 LOGFILE="logs/log-digest-$NOW.log"
 
-python3.8.2 send_digest.py > $LOGFILE
+python3.8.2 send_digest.py > $LOGFILE 2>&1
