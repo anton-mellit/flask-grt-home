a04a1013490ed71d203831951017551b992792a2
anton-mellit <mellit@gmail.com>
2020-05-10 01:15:25 +0200

    Send emails without math

--- emails.py
+++ emails.py
@@ -1,4 +1,4 @@
-from run import mail, config, markdown
+from run import app, mail, config, markdown
 
 from flask_mail import Message
 
@@ -9,6 +9,9 @@ text_maker.ignore_links = True
 
 default_sender = tuple(config['email']['default_sender'])
 default_recipient = tuple(config['email']['default_recipient'])
+
+from flaskext.markdown import Markdown
+markdown_no_math = Markdown(app, extensions=['extra'])
 
 def send_email(body_md, sender=None, recipients=None):
     body_md = body_md.strip()
@@ -22,7 +25,6 @@ def send_email(body_md, sender=None, recipients=None):
     msg = Message(subject)
     msg.recipients = recipients
     msg.sender = sender
-    msg.html = markdown(body_md)
+    msg.html = markdown_no_math(body_md)
     msg.body = text_maker.handle(msg.html)
     mail.send(msg)
-
