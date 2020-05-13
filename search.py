8886977916c9484a031b7cad963fb7051af33fc1
anton-mellit <mellit@gmail.com>
2020-05-13 10:58:15 +0200

    fix indexing of dates

--- search.py
+++ search.py
@@ -10,11 +10,16 @@ from pages import PageCollection, folders
 
 from whoosh.qparser import QueryParser
 
+import datetime
+
 schema = Schema(url=ID(stored=True, unique=True), date=DATETIME(stored=True),
     seminar=TEXT(stored=True), talk_speaker=TEXT(stored=True),
     title=TEXT(stored=True), talk_title=TEXT(stored=True), talk_location=TEXT(stored=True),
     speaker_affiliation=TEXT(stored=True), entered_date=DATETIME(stored=True),
     content=TEXT(stored=True))
+
+def date_to_datetime(dt):
+    return datetime.datetime.combine(dt, datetime.time())
 
 class Search:
     def __init__(self, app):
@@ -39,6 +44,8 @@ class Search:
             for field, field_type in schema.items():
                 if field in item:
                     doc[field] = item[field]
+                    if isinstance(doc[field], datetime.date):
+                        doc[field] = date_to_datetime(doc[field])
                     if isinstance(field_type, TEXT):
                         content.append(doc[field])
             if 'video' in item:
